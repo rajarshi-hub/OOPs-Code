2181de381901817d0a9405034303d2d609a97a81
Rajarshi Tiwari <62015785+rajarshi-hub@users.noreply.github.com>
2021-12-19 19:15:36 +0530

    Create friend_func.cpp

---
+++ friend_func.cpp
@@ -0,0 +1,45 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+class A;
+class B
+{
+    int a;
+    public:
+    int x = 5;
+    void friendly(A);
+    
+};
+class A{
+    int y;
+    public:
+    friend void func(A obj);
+    friend void B::friendly(A); 
+    void setval(int val)
+    {
+        y = val;
+    }
+};
+void B::friendly(A obj)                                    //Function of Class B -- Friend Function of Class A
+{
+    cout<<"Friend Function of B Class"<<" "<<obj.y;        //Access Private Data Member
+}
+void func(A obj){                                          // Global Friend Function
+    cout<<"Global Friend Function"<<" "<<obj.y;            // Access private data Member
+}
+int main() 
+{
+    A obj;
+    B obj2;
+    obj.setval(7);
+    func(obj);
+    cout<<endl;
+    obj2.friendly(obj);
+}
